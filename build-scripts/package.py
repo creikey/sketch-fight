d768b77d74d0a05619095fdaf494a6a208b3282d
Cameron Reikes <cameronreikes@gmail.com>
2019-08-15 10:20:40 -0700

    Add godot native lib generation

--- build-scripts/package.py
+++ build-scripts/package.py
@@ -20,12 +20,26 @@ output_directory = sys.argv[4]
 print(f"output directory: {output_directory}")
 game_name = sys.argv[5]
 editor_settings_path = os.path.expanduser(sys.argv[6])
+gdnative_library_path = sys.argv[7]
 
-# if not os.path.exists(output_directory):
-#     os.makedirs(output_directory)
+def ensure_directory(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
 if not os.path.exists(editor_settings_path):
     os.mknod(editor_settings_path)
+
+# copy build outputs
+native_output_path = (platform_name, os.path.join(source_directory, "build"))
+windows_output_path = ("windows", os.path.join(source_directory, "build-windows"))
+mac_output_path = ("mac", os.path.join(source_directory, "build-mac"))
+linux_output_path = ("linux", os.path.join(source_directory, "build-linux"))
+for p in [native_output_path, windows_output_path, mac_output_path, linux_output_path]:
+    print("checking path " + p[1])
+    if os.path.exists(p[1]):
+        output_folder = os.path.join(source_directory, "src", "bin", p[0])
+        ensure_directory(output_folder)
+        shutil.copy2(gdnative_library_path, output_folder)
 
 editor_settings_cache_directory = tempfile.TemporaryDirectory()
 editor_settings_cache_path = os.path.join(
