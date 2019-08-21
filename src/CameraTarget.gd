1aa9659c6cf6c12fc84242f426b6056060f5e0ed
Cameron Reikes <cameronreikes@gmail.com>
2019-08-21 09:19:29 -0700

    Replace broken grid implementation with dot matrix

--- src/CameraTarget.gd
+++ src/CameraTarget.gd
@@ -12,11 +12,12 @@ func _process(delta):
 	var output_zoom = ((target_zoom - $Camera2D.zoom.x) * c) + $Camera2D.zoom.x
 	$Camera2D.zoom.x = output_zoom
 	$Camera2D.zoom.y = output_zoom
+	print(output_zoom)
 
 func _input(event):
 	if event.is_action("ui_zoom_in"):
 		target_zoom -= zoom_amount
-		target_zoom = clamp(target_zoom, 0.1, 10.0)
+		target_zoom = clamp(target_zoom, 0.5, 4.0)
 	elif event.is_action("ui_zoom_out"):
 		target_zoom += zoom_amount
-		target_zoom = clamp(target_zoom, 0.1, 10.0)
+		target_zoom = clamp(target_zoom, 0.5, 4.0)
