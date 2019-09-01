a3c9e30d6525fa17c9bd12ed83af45e7851a5d3b
Cameron Reikes <cameronreikes@gmail.com>
2019-09-01 00:32:15 -0700

    Add resources when driving over

--- src/ResourceBlock.gd
+++ src/ResourceBlock.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 export var cooldown_time = 10.0
+export var resource_yield = 10.0
 
 onready var initial_a = $Sprite.modulate.a
 
 func _on_ResourceBlock_body_entered(body):
 	if body is Ship and not $AnimationPlayer.is_playing():
 		$AnimationPlayer.play("resources_gotten")
+		Lobby.player_resources[body.get_network_master()] += resource_yield
